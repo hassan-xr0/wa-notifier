d1323490a26fb40ef38f8077ae5d0f22d679fcf0
hassan-xr0 <syedhassan3038@gmail.com>
2026-02-15 13:27:52 +0500

    removed archive notification

--- notifier.js
+++ notifier.js
@@ -4,6 +4,13 @@ const { spawn } = require('child_process');
 
 const SOUND_FILE = '/home/str/.local/share/wa-notifier/assets/Fear-to-fathom.mp3';
 
+function formatDuration(seconds) {
+    const m = Math.floor(seconds / 60);
+    const s = seconds % 60;
+    return `${m}:${s.toString().padStart(2, '0')}`;
+}
+
+// This tell the whatsap-chromium to run in background  without using any resource
 const client = new Client({
   authStrategy: new LocalAuth({
     dataPath: './session'
@@ -20,19 +27,43 @@ const client = new Client({
   }
 });
 
+
+// Login qrcode
 client.on('qr', (qr) => {
   console.log('\nScan this QR code:\n');
   qrcode.generate(qr, { small: true });
-});
+}
+);
 
+// Tells the when the notifier is ready
 client.on('ready', () => {
   console.log('WhatsApp notifier is ready.');
 });
 
-client.on('message', (message) => {
 
-  const sender = message._data.notifyName || message.from;
-  const text = message.body ? message.body.slice(0, 200) : '<media>';
+// Perform the task when messages recieved
+client.on('message',async (message) => {
+  const chat = await message.getChat();
+  // const media = awaitgcc message.downloadMedia();
+  if (chat.archived) return; // skip archived chats
+  console.log(message)
+
+  const sender = `  ${message._data.notifyName || message.from}`;
+  let text;
+  
+  if(message.type === "image"){
+    const sizeKB = Math.round((message._data.size * 3 / 4) / 1024); // approximate size in KB
+    text = `  Image ${sizeKB} KB`;
+
+  }
+  else if(message.type === "ptt" || message.type === "audio"){
+    const duration = message.duration || 0; // in seconds
+    text = `  ${formatDuration(duration)}`;
+  }
+  else{
+    text=message.body
+  } 
+  
 
   // 🔔 Play sound
   spawn('mpv', [SOUND_FILE]);
@@ -44,6 +75,14 @@ client.on('message', (message) => {
     'WhatsApp',
     `${sender}: ${text}`
   ]);
-});
+}
+);
+
+
+// not supported yet on linux
+// client.on('call', (call) => {
+//   console.log(call.status);
+// });
+//
 
 client.initialize();
